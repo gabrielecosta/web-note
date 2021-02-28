b8221996a6840e624b2029b9afd093096a59db4b
gabrielecosta <67752197+gabrielecosta@users.noreply.github.com>
2021-02-28 12:39:07 +0100

    updated routing-module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
-import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AuthguardGuard } from './authguard.guard';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { NgModule } from '@angular/core';
+import { RegisterComponent } from './register/register.component';
+
+const routes: Routes = [
+{ path: '', component: LoginComponent },
+{ path: 'login', component: LoginComponent },
+{ path: 'home', component: HomeComponent },
+{ path: 'registration', component: RegisterComponent },
+{ path: 'dashboard', component: DashboardComponent,canActivate: [AuthguardGuard] }
+
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+imports: [RouterModule.forRoot(routes)],
+exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
