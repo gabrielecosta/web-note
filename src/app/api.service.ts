3446d9b3febbdfe0eccce1480f80e6839e08c6bb
gabrielecosta <67752197+gabrielecosta@users.noreply.github.com>
2021-03-02 12:58:34 +0100

    refactoring of service and dashboard

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,7 +1,8 @@
-import { Injectable, Output, EventEmitter } from '@angular/core';
-import { map } from 'rxjs/operators';
+import { EventEmitter, Injectable, Output } from '@angular/core';
+
 import { HttpClient } from '@angular/common/http';
 import { Users } from '../model/users';
+import { map } from 'rxjs/operators';
 
 @Injectable({
 providedIn: 'root'
@@ -10,40 +11,52 @@ providedIn: 'root'
 export class ApiService {
 redirectUrl: string = '/dashboard';
 baseUrl: string = "http://localhost/login_CRUD/angular/php";
+
 @Output() getLoggedInName: EventEmitter<any> = new EventEmitter();
+
 constructor(private httpClient: HttpClient) { }
+
 public userlogin(username: String, password: String) {
-alert(username)
-return this.httpClient.post<any>(this.baseUrl + '/login.php', { username, password })
-.pipe(map(Users => {
-this.setToken(Users[0].name);
-this.getLoggedInName.emit(true);
-return Users;
-}));
+  //alert(username)
+  return this.httpClient.post<any>(this.baseUrl + '/login.php', { username, password })
+    .pipe(map(Users => {
+      this.setToken(Users[0]);
+      this.getLoggedInName.emit(true);
+      return Users;
+    }));
 }
 
 public userregistration(name: String, email: String, pwd: String) {
-return this.httpClient.post<any>(this.baseUrl + '/register.php', { name,email, pwd })
-.pipe(map(Users => {
-return Users;
-}));
+  return this.httpClient.post<any>(this.baseUrl + '/register.php', { name,email, pwd })
+    .pipe(map(Users => {
+      return Users;
+    }));
 }
 
-//token
-setToken(token: string) {
-localStorage.setItem('token', token);
+//set Token: when the user logs, his name (that will be used as a key) will be
+//stored on a localStorage. So, we can do a fitleredSearch in a second moment
+setToken(token: Users) {
+  localStorage.setItem('token', token.name);
 }
+
+
+//it is used as a method for getting the user
 getToken() {
-return localStorage.getItem('token');
+  return localStorage.getItem('token');
 }
+
+//it deletes the token when the user clicks logout
 deleteToken() {
-localStorage.removeItem('token');
+  localStorage.removeItem('token');
 }
+
+
+//checks if the token is null or not
 isLoggedIn() {
-const usertoken = this.getToken();
-if (usertoken != null) {
-return true
-}
-return false;
-}
+  const usertoken = this.getToken();
+    if (usertoken != null) {
+      return true
+    }
+    return false;
+    }
 }
